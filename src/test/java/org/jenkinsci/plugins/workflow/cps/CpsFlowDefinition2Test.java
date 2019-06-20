b2f88527effb78637e108f65842595b859b6a6c9
Karl Shultz <kshultz@cloudbees.com>
2019-06-20 08:25:41 -0400

    Revert "Un-ignore the two tests in a branch based on master"
    This reverts commit 08bbfdbb4242a2d5cf2b70963a99dd2ea420e806.

--- src/test/java/org/jenkinsci/plugins/workflow/cps/CpsFlowDefinition2Test.java
+++ src/test/java/org/jenkinsci/plugins/workflow/cps/CpsFlowDefinition2Test.java
@@ -43,7 +43,6 @@ import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.ClassRule;
 import org.junit.Ignore;
-import org.junit.
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
@@ -86,8 +85,8 @@ public class CpsFlowDefinition2Test extends AbstractCpsFlowTest {
      * Verify that we kill endlessly recursive CPS code cleanly.
      */
     @Test
-    // @Ignore /** Intermittent failures because triggers a longstanding unrelated SandboxResolvingClassloader bug
-    // resolved in https://github.com/jenkinsci/script-security-plugin/pull/160 */
+    @Ignore /** Intermittent failures because triggers a longstanding unrelated SandboxResolvingClassloader bug
+     resolved in https://github.com/jenkinsci/script-security-plugin/pull/160 */
     public void endlessRecursion() throws Exception {
         Assume.assumeTrue(!Functions.isWindows());  // Sidestep false failures specific to a few Windows build environments.
         String script = "def getThing(){return thing == null}; \n" +
@@ -116,8 +115,8 @@ public class CpsFlowDefinition2Test extends AbstractCpsFlowTest {
      *  we don't trigger other forms of failure with the StackOverflowError.
      */
     @Test
-    // @Ignore  /** Intermittent failures because triggers a longstanding unrelated SandboxResolvingClassloader bug
-    //              resolved in https://github.com/jenkinsci/script-security-plugin/pull/160 */
+    @Ignore  /** Intermittent failures because triggers a longstanding unrelated SandboxResolvingClassloader bug
+                 resolved in https://github.com/jenkinsci/script-security-plugin/pull/160 */
     public void endlessRecursionNonCPS() throws Exception {
         Assume.assumeTrue(!Functions.isWindows());  // Sidestep false failures specific to a few Windows build environments.
 
